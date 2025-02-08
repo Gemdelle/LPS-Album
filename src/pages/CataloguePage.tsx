510020065f63bf2ca34a1d105880da583622a4eb
Gemdelle <milu200911@hotmail.com>
2025-02-07 22:37:33 -0300

    catalogue button.

--- src/pages/CataloguePage.tsx
+++ src/pages/CataloguePage.tsx
@@ -88,6 +88,9 @@ const CataloguePage = ({ setLocation, data, updateGoogleSheet, refreshData }: an
                 <button className='view-button' onClick={() => setUseCardView(!useCardView)}>
                     {useCardView ? "Cards" : "Catalogue"}
                 </button>
+                {/* <button className='view-button'>
+                    button 2
+                </button> */}
             </div>
             
             {catalogueData.map((petshop: any) => {
